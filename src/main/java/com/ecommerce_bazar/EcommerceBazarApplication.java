4c1b4ef394c8702443c9ff503e7d93b5b678259e
anoop18 <Umang@1234567890>
2026-09-15 08:34:08 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce_bazar/EcommerceBazarApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce_bazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBazarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce_bazar/EcommerceBazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_bazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBazarApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
